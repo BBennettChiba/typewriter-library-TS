3bd419e75a56af8565d2929d85bef2f878c9366a
BBennettChiba <BrysonCBennett@gmail.com>
2022-05-11 13:20:02 +0900

    added stuff, didnt follow guide

--- src/Typewriter.ts
+++ src/Typewriter.ts
@@ -3,16 +3,43 @@ export default class Typewriter {
   loop: boolean;
   typingSpeed: number;
   eraseSpeed: number;
+  #buffer: Array<Function> = [];
 
-  constructor(element: HTMLElement, { loop = true, typingSpeed = 100, eraseSpeed = 100 }) {
+  constructor(
+    element: HTMLElement,
+    { loop = true, typingSpeed = 100, eraseSpeed = 100 } = {}
+  ) {
     this.element = element;
     this.loop = loop;
     this.typingSpeed = typingSpeed;
     this.eraseSpeed = eraseSpeed;
   }
   typeString(string: string) {
+    this.#buffer.push(() => {
+      this.element.innerText += string;
+    });
+    return this;
   }
-  pauseFor(ms: number) {}
-  eraseString(charCount: number) {}
-  start() {}
+  pauseFor(ms: number) {
+    this.#buffer.push(() => {
+      setTimeout(() => {}, ms);
+    });
+    return this;
+  }
+  deleteChars(charCount: number) {
+    this.#buffer.push(() => {
+      this.element.innerText = this.element.innerText.slice(0, -charCount);
+    });
+    return this;
+  }
+
+  deleteAll(speed = this.eraseSpeed) {
+    this.#buffer.push(() => {
+      this.element.innerText = "";
+    });
+    return this;
+  }
+  start() {
+    this.#buffer.forEach((fn) => fn());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import './style.css'
-import Typewriter from './Typewriter';
+import "./style.css";
+import Typewriter from "./Typewriter";
 
-const typewriter = new Typewriter(document.body, {loop: true});
+const typewriter = new Typewriter(document.body, { loop: true });
 
-typewriter.typeString('Hello World!')
-.pauseFor(100)
-.typeString("\n\nfunction")
-.deleteAll(10)
-.start()
+typewriter
+  .typeString("Hello World!")
+  .pauseFor(100)
+  .typeString("\n\nfunction")
+  .deleteChars(5)
+  .typeString("butts")
+  //.deleteAll()
+  .start();
